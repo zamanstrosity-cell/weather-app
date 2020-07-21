10cde9f161d6255024c5c2a5801884def2ac66a7
Omar Mansour <omarsayedmohamedmansour@gmail.com>
2020-07-21 18:30:27 +0200

    Updates

---
+++ precache-manifest.7005fcfe12a4e0aee9de3273b3974979.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3162becf3a7284e4caa847fc121635ed",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "ea3978e0d464aa94dbbc",
+    "url": "/weather-app/static/css/main.c035b177.chunk.css"
+  },
+  {
+    "revision": "8d2553961f42f0a648fb",
+    "url": "/weather-app/static/js/2.d21ffc0f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-app/static/js/2.d21ffc0f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea3978e0d464aa94dbbc",
+    "url": "/weather-app/static/js/main.62bb1481.chunk.js"
+  },
+  {
+    "revision": "d0e2e579b5155f255804",
+    "url": "/weather-app/static/js/runtime-main.e6f382d7.js"
+  }
+]);
