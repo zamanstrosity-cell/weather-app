1ccd5913424e9219eec4a6b43babb9422b7b29af
Omar Mansour <omarsayedmohamedmansour@gmail.com>
2020-07-21 18:25:49 +0200

    fixed colors

--- src/components/Card.js
+++ src/components/Card.js
@@ -16,7 +16,7 @@ export class Card extends Component {
             flexDirection: "column",
             alignItems: "center",
             padding: "20px",
-            backgroundColor: "#8EC5FC"
+            backgroundImage: "linear-gradient(62deg, #8EC5FC 0%, #E0C3FC 100%)"
         
         }
         return (
